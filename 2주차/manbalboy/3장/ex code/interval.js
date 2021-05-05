a1c27dc4b7ec7ae55cddab5ea6e8e3ed6120fc8d
manbalboy <manbalboy@hanmail.net>
2021-05-05 14:42:07 +0900

    [2주차][manbalboy] timer 예제소스 추가

---
+++ 2주차/manbalboy/3장/ex code/interval.js
@@ -0,0 +1,3 @@
+const interval = setInterval(() => {
+    console.log('interval 1초후 실행');
+}, 1000);
---
+++ 2주차/manbalboy/3장/ex code/timeout.js
@@ -0,0 +1,6 @@
+const timeout = setTimeout(() => {
+    console.log('timeout 1초후 실행');
+}, 1000);
+
+// 바로 취소하려면 아래의 주석을 푸세요
+// clearTimeout(timeout);
